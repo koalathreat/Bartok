7b6f97b5e533be451b0cc49766fc5ec317b43e6b
koalathreat <edwin.zheng@rutgers.edu>
2017-11-18 04:32:01 -0500

    Final Results
    Completed the assignment. Only problem is that the game doesn't run due
    to an "unassigned reference exception". I doublechecked all my code and
    the instructions but couldn't find what was causing the issue.

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card {
+
+    public CardState state = CardState.drawpile;
+    // The hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // LayoutID matches this card to a Layout XML id if it's a tableau card
+    public int layoutID;
+    // The SlotDef class stores information pulled in from the LayoutXML <slot>
+    public SlotDef slotDef;
+
+    override public void OnMouseUpAsButton()
+    {
+        // Call the CardClicked method on the Prospector singleton
+        Prospector.S.CardClicked(this);
+        // Also call the base class (Card.cs) version of this method
+        base.OnMouseUpAsButton();
+    }
+}
